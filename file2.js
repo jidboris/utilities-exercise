ac6197cf82039dfba3be4200db112210ffeab45d
Olajide Borisade <102518345+jidboris@users.noreply.github.com>
2022-05-13 22:42:05 +0100

    Add files via upload

---
+++ file2.js
@@ -0,0 +1,14 @@
+// add numbers in an array
+const sum = (arr) => {
+    const total = 0;
+    arr.forEach((x) => {
+        total += x;
+    })
+    return total
+}
+
+// concatenate two arrays
+const concat = (arr1, arr2) => {
+    return [...arr1, ...arr2]
+}
+
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const fs = require('fs')
+const writeStream = fs.createWriteStream('./utilityFile.txt')
+for (let i = 0; i < 10; i++) {
+    writeStream.write('i am a file\n')
+}
+try{
+let dataFromFile = fs.readFileSync('utilityFile.txt')
+console.log('data', dataFromFile.toString())
+}
+catch(e){
+console.log('Error', e)
+}
 
